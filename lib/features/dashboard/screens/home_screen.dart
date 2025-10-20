bab4e241b6bd82bae03ca1e8d5225c63d2414e29
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-20 22:36:42 +0600

    Step 2: Create the Home Screen This will be the main screen that holds my bottom navigation bar
    Why a StatefulWidget?
    
    The HomeScreen needs to remember which tab is currently selected. This selected tab index (_selectedIndex) is its "state." A StatefulWidget is perfect for UI that needs to change based on user interaction or internal data.
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

---
+++ lib/features/dashboard/screens/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  // A list of placeholder widgets for each tab
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Home / Dashboard')),
+    Center(child: Text('Mentors')),
+    Center(child: Text('Profile')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CampusGuardian'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Mentors',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
