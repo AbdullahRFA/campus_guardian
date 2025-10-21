833e2fd4887241d6b1b322a6a7f35fe519812649
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-21 23:42:03 +0600

    Make the AuthGate Intelligent:
    Now for the magic. We'll make the AuthGate listen to Firebase's authentication
     state. When a user logs in, it will automatically redirect them to the
     main app dashboard.
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

--- lib/features/auth/screens/auth_gate.dart
+++ lib/features/auth/screens/auth_gate.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'login_screen.dart';
 
 class AuthGate extends StatelessWidget {
@@ -6,9 +8,28 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // For now, we always show the login screen.
-    // Tomorrow, we'll add logic here to check if the user
-    // is actually logged in with Firebase.
-    return const LoginScreen();
+    return StreamBuilder<User?>(
+      // Listen to the authentication state changes from Firebase
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Show a loading indicator while checking the auth state
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+
+        // This callback runs after the widget tree is built.
+        // It safely navigates based on the auth state.
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (snapshot.hasData) {
+            // If user is logged in (snapshot has data), go to the main app.
+            context.go('/app/dashboard');
+          }
+        });
+
+        // If user is not logged in (or we are about to navigate away),
+        // show the LoginScreen. This is the default state.
+        return const LoginScreen();
+      },
+    );
   }
 }
