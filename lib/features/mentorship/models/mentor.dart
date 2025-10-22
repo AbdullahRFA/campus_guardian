4fb1c3d5ba11d8b8ab2286827160b007ae34f290
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-22 12:15:50 +0600

    Create the Mentor Data Model:
    A "model" is a class that defines the structure of our data. It ensures that every mentor object in our app has the same properties (like name, title, etc.).
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

---
+++ lib/features/mentorship/models/mentor.dart
@@ -0,0 +1,17 @@
+class Mentor {
+  final String id;
+  final String name;
+  final String title;
+  final String company;
+  final String profileImageUrl;
+  final List<String> expertise;
+
+  Mentor({
+    required this.id,
+    required this.name,
+    required this.title,
+    required this.company,
+    required this.profileImageUrl,
+    required this.expertise,
+  });
+}
