a7ad58724941bd96a30909a326a6b0ae026f4f83
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-22 23:40:59 +0600

    Update the MentorListScreen to Use Live Data:
    This is the most important step. We'll replace the dummy list with a StreamBuilder that listens for real-time updates from Firestore.
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

--- lib/features/mentorship/screens/mentor_list_screen.dart
+++ lib/features/mentorship/screens/mentor_list_screen.dart
@@ -1,24 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../models/mentor.dart';
 import '../widgets/mentor_card.dart';
 
 class MentorListScreen extends StatelessWidget {
-  // MODIFIED: Added a final variable to hold the list of mentors.
-  final List<Mentor> mentors;
-
-  // MODIFIED: Updated the constructor to accept the 'mentors' parameter.
-  const MentorListScreen({super.key, required this.mentors});
+  const MentorListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Find a Mentor')),
-      body: ListView.builder(
-        // Use the 'mentors' list that was passed in.
-        itemCount: mentors.length,
-        itemBuilder: (context, index) {
-          final mentor = mentors[index];
-          return MentorCard(mentor: mentor);
+      // Use a StreamBuilder to listen for live data
+      body: StreamBuilder<QuerySnapshot>(
+        // The query: get all users who have marked themselves as available
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('isMentorAvailable', isEqualTo: true)
+            .snapshots(),
+
+        builder: (context, snapshot) {
+          // Show a loading indicator while data is being fetched
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          // Show an error message if something goes wrong
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong.'));
+          }
+          // Show a message if no mentors are available
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No mentors are available right now.'));
+          }
+
+          // If we have data, get the list of documents
+          final mentorDocs = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: mentorDocs.length,
+            itemBuilder: (context, index) {
+              // Create a Mentor object from each document
+              final mentor = Mentor.fromFirestore(mentorDocs[index]);
+              return MentorCard(mentor: mentor);
+            },
+          );
         },
       ),
     );
