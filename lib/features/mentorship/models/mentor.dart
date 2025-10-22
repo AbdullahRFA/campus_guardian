49364e5600507dc4beccee1786cb357138fe25e7
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-22 23:39:45 +0600

    Update the Mentor Model:
    we need to teach our Mentor model how to be created from a Firestore document instead of just hardcoded text.
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

--- lib/features/mentorship/models/mentor.dart
+++ lib/features/mentorship/models/mentor.dart
@@ -1,18 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Mentor {
   final String id;
   final String name;
   final String title;
-  final String company;
   final String profileImageUrl;
   final List<String> expertise;
 
-  // Add 'const' here
   const Mentor({
     required this.id,
     required this.name,
     required this.title,
-    required this.company,
     required this.profileImageUrl,
     required this.expertise,
   });
+
+  // NEW: A factory constructor to create a Mentor from a Firestore document
+  factory Mentor.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Mentor(
+      id: doc.id,
+      name: data['fullName'] ?? 'No Name',
+      title: data['mentorTitle'] ?? 'Mentor',
+      profileImageUrl: data['profilePicUrl'] ?? '',
+      expertise: List<String>.from(data['mentorExpertise'] ?? []),
+    );
+  }
 }
