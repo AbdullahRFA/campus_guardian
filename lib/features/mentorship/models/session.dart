d79f5f08caaa0980b12c1a65e44fc736d4a38a82
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-23 10:47:13 +0600

    Create the Session Card Widget
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

---
+++ lib/features/mentorship/models/session.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Session {
+  final String mentorName;
+  final String menteeName;
+  final String mentorId;
+  final String menteeId;
+  final String sessionTime;
+  final String sessionDate;
+  final String status;
+
+  Session({
+    required this.mentorName,
+    required this.menteeName,
+    required this.mentorId,
+    required this.menteeId,
+    required this.sessionTime,
+    required this.sessionDate,
+    required this.status,
+  });
+
+  factory Session.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Session(
+      mentorName: data['mentorName'] ?? '',
+      menteeName: data['menteeName'] ?? '',
+      mentorId: data['mentorId'] ?? '',
+      menteeId: data['menteeId'] ?? '',
+      sessionTime: data['sessionTime'] ?? '',
+      sessionDate: data['sessionDate'] ?? '',
+      status: data['status'] ?? '',
+    );
+  }
+}
