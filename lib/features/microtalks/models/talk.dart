dc4242eb383b84520cd76d9f7753cb4e04eaca54
AbdullahRFA <shakibrybmn@gmail.com>
2025-10-23 22:59:08 +0600

    Create the Talk Data Model:
    we need to define the data structure for a single Micro-Talk.
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

---
+++ lib/features/microtalks/models/talk.dart
@@ -0,0 +1,15 @@
+class Talk {
+  final String id;
+  final String title;
+  final String speakerName;
+  final String duration;
+  final String thumbnailUrl;
+
+  const Talk({
+    required this.id,
+    required this.title,
+    required this.speakerName,
+    required this.duration,
+    required this.thumbnailUrl,
+  });
+}
